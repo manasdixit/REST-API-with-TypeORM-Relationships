e99da4437551225104bbb1c5a1993301f733710f
manasdixit <manas.dixit.std@gmail.com>
2021-10-28 16:28:11 +0530

    removed unnecessary app.services file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeOrmConfig from 'ormconfig';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CompanyController } from './company/company.controller';
 import { EmployeeController } from './employee/employee.controller';
 import { TaskController } from './task/task.controller';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [AppController, CompanyController, EmployeeController, TaskController],
-  providers: [AppService],
+   imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+   controllers: [
+      AppController,
+      CompanyController,
+      EmployeeController,
+      TaskController,
+   ],
+   providers: [],
 })
 export class AppModule {}
